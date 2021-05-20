94a43f201e092b6ae717078d3f8ce757aaded0bb
b0yank <bnkaloyanov@gmail.com>
2021-05-20 12:53:11 +0300

    Uncommented select tests; Added cypress run on npm test command

--- cypress/integration/commands/select/select.spec.js
+++ cypress/integration/commands/select/select.spec.js
@@ -39,60 +39,60 @@ describe('Elements select command', () => {
         pressEscape()
     }
     
-    // it('should select elements by single click on them', () => {
-    //     drawElements()
+    it('should select elements by single click on them', () => {
+        drawElements()
 
-    //     click(150, 50)
-    //     click(100, 400)
-    //     click(100, 50)
-    //     click(400, 100)
-    //     click(500, 400)
-    // })
+        click(150, 50)
+        click(100, 400)
+        click(100, 50)
+        click(400, 100)
+        click(500, 400)
+    })
 
-    // it('should select elements by single click on them (other points)', () => {
-    //     drawElements()
+    it('should select elements by single click on them (other points)', () => {
+        drawElements()
 
-    //     click(200, 50)
-    //     click(50, 350)
-    //     click(225, 140)
-    //     click(470.71, 270.71)
-    //     click(600, 600)
-    // })
+        click(200, 50)
+        click(50, 350)
+        click(225, 140)
+        click(470.71, 270.71)
+        click(600, 600)
+    })
 
-    // it('should select elements by partial select window', () => {
-    //     drawElements()
+    it('should select elements by partial select window', () => {
+        drawElements()
 
-    //     click(60, 10)
-    //     click(20, 80)
+        click(60, 10)
+        click(20, 80)
 
-    //     click(170, 180)
-    //     click(130, 240)
+        click(170, 180)
+        click(130, 240)
 
-    //     click(130, 450)
-    //     click(110, 380)
+        click(130, 450)
+        click(110, 380)
 
-    //     click(540, 230)
-    //     click(470, 140)
-    // })
+        click(540, 230)
+        click(470, 140)
+    })
 
-    // it('should select elements by partial select window (other selection windows)', () => {
-    //     drawElements()
+    it('should select elements by partial select window (other selection windows)', () => {
+        drawElements()
 
-    //     click(260, 30)
-    //     click(10, 70)
+        click(260, 30)
+        click(10, 70)
 
-    //     click(70, 380)
-    //     click(30, 420) // should NOT select arc by center
+        click(70, 380)
+        click(30, 420) // should NOT select arc by center
 
-    //     click(290, 230)
-    //     click(210, 160)
+        click(290, 230)
+        click(210, 160)
 
-    //     click(330, 260)
-    //     click(260, 340)
+        click(330, 260)
+        click(260, 340)
 
-    //     click(700, 500)
-    //     click(550, 400)
-    // })
+        click(700, 500)
+        click(550, 400)
+    })
 
     it('should not select partial elements with whole-select window', () => {
         drawElements()
