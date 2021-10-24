ecab77e9f29253cb6c6cd2452ca4eb9e89f6b5ec
b0yank <bnkaloyanov@gmail.com>
2021-10-24 22:48:16 +0300

    Further work on improving Hash Grid

--- src/utils/hashGrid/extensions.js
+++ src/utils/hashGrid/extensions.js
@@ -1,5 +1,9 @@
 // NOTE: For extension functions to work, this must be bound to an object of type HashGrid
 
+import { MAX_NUM_ERROR } from '../constants'
+import { createPoint } from '../elementFactory'
+import { getPointDistance } from '../point'
+import PriorityQueue from '../priorityQueue'
 import SetUtils from '../setUtils'
 import { getDimensionDivision1d } from './utils'
 
@@ -11,6 +15,48 @@ function getLineDivs(line) {
 
     const [minX, maxX] = [startPointX, endPointX].sort((a, b) => a - b)
     const [minY, maxY] = [startPointY, endPointY].sort((a, b) => a - b)
+
+    const queue = new PriorityQueue((entryA, entryB) => {
+        const distanceDiff = entryB.distanceFromStart - entryA.distanceFromStart
+
+        // if false, assume distanceDiff is due to numeric rounding errors
+        const isDistanceDiffSignificant = Math.abs(distanceDiff) > MAX_NUM_ERROR
+        return isDistanceDiffSignificant ? distanceDiff : 0
+    })
+
+    const { slope, intercept } = line.equation
+
+    const firstXLineDiv = Math.ceil(minX / this.divSizeX) || 0 // assume value of 0 in case -0 is returned
+    const lastXLineDiv = Math.floor(maxX / this.divSizeX) || 0 // assume value of 0 in case -0 is returned
+    for (let xLineDiv = firstXLineDiv; xLineDiv <= lastXLineDiv; xLineDiv++) {
+        const lineXDim = xLineDiv * this.divSizeX
+
+        const lineIntersectionY = slope * lineXDim + intercept
+        const intersectionPoint = createPoint(lineXDim, lineIntersectionY)
+        const intersectDistanceFromStart = getPointDistance(intersectionPoint, startPoint)
+        queue.push({ 
+            point: intersectionPoint, 
+            distanceFromStart: intersectDistanceFromStart, 
+            isCrossingX: true 
+        })
+    } 
+
+    const firstYLineDiv = Math.ceil(minY / this.divSizeY) || 0 // assume value of 0 in case -0 is returned
+    const lastYLineDiv = Math.floor(maxY / this.divSizeY) || 0 // assume value of 0 in case -0 is returned
+    for (let yLineDiv = firstYLineDiv; yLineDiv <= lastYLineDiv; yLineDiv++) {
+        const lineYDim = yLineDiv * this.divSizeY
+
+        // TODO: What if slope is 0 or NaN?
+        const lineIntersectionX = (lineYDim - intercept) / slope 
+        const intersectionPoint = createPoint(lineIntersectionX, lineYDim)
+
+        const intersectDistanceFromStart = getPointDistance(intersectionPoint, startPoint)
+        queue.push({ 
+            point: intersectionPoint, 
+            distanceFromStart: intersectDistanceFromStart, 
+            isCrossingX: false 
+        })
+    } 
 
     // TODO: loop through x and y divisions, get interceptions of line and add them to a 
     // priority queue based on distance from startPoint. Find out how to merge intercepts 
